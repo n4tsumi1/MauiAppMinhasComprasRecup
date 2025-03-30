14625eab052092ada11ed29ab6b2c4044fbe8b21
Erika <eaq990@gmail.com>
2025-03-30 15:32:56 -0300

    Adicionando funcionalidade de inserção no SQLite

--- MauiAppMinhasComprasRecup/App.xaml.cs
+++ MauiAppMinhasComprasRecup/App.xaml.cs
@@ -1,8 +1,28 @@
-﻿namespace MauiAppMinhasComprasRecup
-    
-{
+﻿using MauiAppMinhasComprasRecup.Helpers;
+
+namespace MauiAppMinhasComprasRecup
+{ 
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if(_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqLite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+                
+                return _db;
+            }
+        }
         public App()
         {
             InitializeComponent();
@@ -11,4 +31,5 @@
             MainPage = new NavigationPage(new Views.ListaProduto());
         }
     }
+
 }
